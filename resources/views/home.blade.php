2b6ad2dde59f8cb95ae9be04dc6051f0c986beed
mrglide <wdf.glencliff@gmail.com>
2016-08-08 19:43:07 +0000

    Fixed CSS style att code error Home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,9 +42,9 @@ There are no announcements.  Please log in and post a new announcement.
 			<h3><a href="{{ url('/'.$post->slug) }}">{{ $post->title }}</a>
 				@if(!Auth::guest() && ($post->author_id == Auth::user()->id || Auth::user()->is_admin()))
 					@if($post->active == '1')
-					<button class="btn" style="float: right"><a href="{{ url('edit/'.$post->slug)}}">Edit Post</a></button>
+					<button class="btn" style="float: right;"><a href="{{ url('edit/'.$post->slug)}}">Edit Post</a></button>
 					@else
-					<button class="btn" style="float: right"><a href="{{ url('edit/'.$post->slug)}}">Edit Draft</a></button>
+					<button class="btn" style="float: right;"><a href="{{ url('edit/'.$post->slug)}}">Edit Draft</a></button>
 					@endif
 				@endif
 			</h3>
