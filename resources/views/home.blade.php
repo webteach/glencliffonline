68cacf827f46921ca6898a543448312b9e48c987
mrglide <wdf.glencliff@gmail.com>
2016-08-08 19:50:16 +0000

    Add type to button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,7 +42,7 @@ There are no announcements.  Please log in and post a new announcement.
 			<h3><a href="{{ url('/'.$post->slug) }}">{{ $post->title }}</a>
 				@if(!Auth::guest() && ($post->author_id == Auth::user()->id || Auth::user()->is_admin()))
 					@if($post->active == '1')
-					<button class="btn" style="float: right;"><a href="{{ url('edit/'.$post->slug)}}">Edit Post</a></button>
+					<button type="submit" class="btn" style="float: right;"><a href="{{ url('edit/'.$post->slug)}}">Edit Post</a></button>
 					@else
 					<button class="btn" style="float: right;"><a href="{{ url('edit/'.$post->slug)}}">Edit Draft</a></button>
 					@endif
