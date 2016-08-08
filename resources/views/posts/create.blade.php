bf788ca0e701cd7f212d3e2e20c485601e210c65
mrglide <wdf.glencliff@gmail.com>
2016-08-08 03:48:46 +0000

    Added /new-posts to form action in create blade

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -14,7 +14,7 @@ Add New Announcement
 	}); 
 </script>
 
-<form action="new-post" method="post">
+<form action="/new-post" method="post">
 	<input type="hidden" name="_token" value="{{ csrf_token() }}">
 	<div class="form-group">
 		<input required="required" value="{{ old('title') }}" placeholder="Enter title here" type="text" name ="title" class="form-control" />
