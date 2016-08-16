89d4203002a4491f46ed57ba40da50b66fcd05ec
mrglide <wdf.glencliff@gmail.com>
2016-08-16 12:59:42 +0000

    Update From address

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => support, 'name' => Glencliffonline_support_team],
 
     /*
     |--------------------------------------------------------------------------
