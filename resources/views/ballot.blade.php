a2eff623899dbcb2823729b54a6448bb75595e81
mrglide <wdf.glencliff@gmail.com>
2016-09-13 12:32:10 +0000

    Fix ballot sizing

--- resources/views/ballot.blade.php
+++ resources/views/ballot.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('content')
 
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScx7psEDQ6NPZYPvYpgoh-dEOwPWsjqwLLHY-svOPFjMj5qZQ/viewform?embedded=true" width="1500px" height="800px" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
-
+<div style="text-align: center;">
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScx7psEDQ6NPZYPvYpgoh-dEOwPWsjqwLLHY-svOPFjMj5qZQ/viewform?embedded=true" width="90%" height="700px" frameborder="0" marginheight="0" marginwidth="0" text-align="center">Loading...</iframe>
+</div>
 @endsection
