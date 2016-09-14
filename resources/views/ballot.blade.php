675f24b98b96255571eff770a6fb62808b401d43
mrglide <wdf.glencliff@gmail.com>
2016-09-14 13:08:06 +0000

    Add new Senior ballot

--- resources/views/ballot.blade.php
+++ resources/views/ballot.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 @section('content')
-<h1>Look for a new ballot tomorrow!</h1>
-<!--
+
+
 <div style="text-align: center;">
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScx7psEDQ6NPZYPvYpgoh-dEOwPWsjqwLLHY-svOPFjMj5qZQ/viewform?embedded=true" width="90%" height="700px" frameborder="0" marginheight="0" marginwidth="0" text-align="center">Loading...</iframe>
+<!--<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScx7psEDQ6NPZYPvYpgoh-dEOwPWsjqwLLHY-svOPFjMj5qZQ/viewform?embedded=true" width="90%" height="700px" frameborder="0" marginheight="0" marginwidth="0" text-align="center">Loading...</iframe>-->
+
+
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfY_SHI3GeWuNVHkLCNQGRhTcbBMIfa7IjujiQShRblK0PCcA/viewform?embedded=true" width="90%" height="700" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
 </div>
--->
 @endsection
